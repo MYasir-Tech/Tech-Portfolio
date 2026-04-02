9d0bf0eeae60fdb0fa40b1329dbcab082a4a68a0
Muhammad Yasir <mailmyasir@gmail.com>
2026-04-02 19:08:25 +0500

    Add basic variables example

---
+++ Python/Python-basics-labs/01_basics/variables.py
@@ -0,0 +1,14 @@
+# Basic variables
+
+name = "Yasir"
+age = "25"
+height = "5.9"
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
